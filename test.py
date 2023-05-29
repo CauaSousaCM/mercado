220c358ac321aefe7abcd3aa7d1c20c3d91f26b3
CauaSousaCM <125994651+DevsQA@users.noreply.github.com>
2023-05-29 15:23:43 -0300

    Final commit

--- test.py
+++
@@ -1,7 +0,0 @@
-from models.products import Product
-
-ps4 = Product('Playstation 4', 1789.44)
-xbox = Product('xbox one', 2000.44)
-
-print(ps4)
-print(xbox)
--- utils/messages.py
+++ utils/messages.py
@@ -1,9 +1,9 @@
-def sucess_message(text: str) -> str:
-    message = (f'\033[32m{text}\033[0m')
-    return message
-
 def error_message(text: str) -> str:
     message = (f'\033[31m{text}\033[0m')
+    return message
+
+def sucess_message(text: str) -> str:
+    message = (f'\033[32m{text}\033[0m')
     return message
 
 def info_message(text: str) -> str:
